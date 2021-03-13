12ae91807a4cfcee47ee91a7335887629e1bc228
Maarten Smeets <wisp1980@gmail.com>
2021-03-13 13:33:17 +0100

    Fix

--- src/main/java/nl/amis/smeetsm/demoservice/controller/DemoRestController.java
+++ src/main/java/nl/amis/smeetsm/demoservice/controller/DemoRestController.java
@@ -1,6 +1,5 @@
 package nl.amis.smeetsm.demoservice.controller;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
