0b56c88a6fd4532bce9989e92476ed6da68458cf
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2021-04-05 11:20:17 +0200

    Update zap_cli_scan.sh

--- .github/zap_cli_scan.sh
+++ .github/zap_cli_scan.sh
@@ -51,7 +51,7 @@ $DOCKER exec $CONTAINER_ID \
 echo "\nGenerating ZAP report"
 $DOCKER exec $CONTAINER_ID \
   zap-cli -p $ZAP_API_PORT report \
-  -o /zap/reports/owaspreport.xml -f xml
+  -o /zap/reports/owaspreport.html -f html
 
 echo "\nCleaning up ZAP"
 # Shut down the docker image
